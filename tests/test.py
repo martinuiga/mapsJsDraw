e72057105c855b668fc6f24a5b8a965b56287f8d
MMartiM <marti.mutso@gmail.com>
2016-10-04 19:21:38 +0300

    test

--- tests/test.py
+++ tests/test.py
@@ -2,14 +2,11 @@ import os
 from selenium import webdriver
 
 driver = webdriver.Firefox()
-driver.get("file://index.html")
+driver.Navigate().GoToRelativePath("/index.html");
 
 # Create a screenshots directory if not present
 # NOTE 2: We are taking screenshots to show CircleCI artifacts
-if not (os.path.exists('./tests/screenshots')):
-    os.makedirs('./tests/screenshots')
-# Save screenshot in the created directory
-driver.save_screenshot('./tests/screenshots/test.png')
+driver.get_screenshot_as_file('screenshots/test.png')
 
 # Assert the Page Title
 driver.find_element_by_id('map')
