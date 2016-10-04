220351c10aacf2088d7e8f5e00c847e73d259fae
MMartiM <marti.mutso@gmail.com>
2016-10-04 20:08:53 +0300

    test

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,8 @@ import os
 from selenium import webdriver
 
 driver = webdriver.Firefox()
-driver.get("file://" + os.path.abspath("../index.html"))
-print("file://" + os.path.abspath("../index.html"));
+driver.get("file:///" + os.path.abspath("../index.html"))
+print("file:///" + os.path.abspath("../index.html"));
 # Create a screenshots directory if not present
 # NOTE 2: We are taking screenshots to show CircleCI artifacts
 driver.get_screenshot_as_file('screenshots/test.png')
