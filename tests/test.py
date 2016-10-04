5951b38ed35e162400e3003a281328fa263d7c2e
MMartiM <marti.mutso@gmail.com>
2016-10-04 20:40:28 +0300

    test

--- tests/test.py
+++ tests/test.py
@@ -3,9 +3,8 @@ from selenium import webdriver
 
 driver = webdriver.Remote("http://localhost:4444/wd/hub", webdriver.DesiredCapabilities.FIREFOX.copy())
 driver.get("file://" + os.path.abspath("index.html"))
-print("file://" + os.path.abspath("index.html"));
 
-driver.get_screenshot_as_file('./screenshots/test.png')
+driver.get_screenshot_as_file(os.path.abspath('screenshots/test.png'))
 assert "Shapefile GMaps" in driver.title
 
 driver.find_element_by_id("map")
