b40ca5a3c1e8c8c69d1e96fd18d7bdf3a8871c2a
MMartiM <marti.mutso@gmail.com>
2016-10-04 19:09:18 +0300

    test

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,7 @@ import os
 from selenium import webdriver
 
 driver = webdriver.Firefox()
-driver.get("index.html")
+driver.get("file://index.html")
 
 # Create a screenshots directory if not present
 # NOTE 2: We are taking screenshots to show CircleCI artifacts
