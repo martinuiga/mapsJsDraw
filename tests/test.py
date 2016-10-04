db90fbeb15e92bd4f256fb82be588f61b3729a6b
MMartiM <marti.mutso@gmail.com>
2016-10-04 20:33:39 +0300

    test

--- tests/test.py
+++ tests/test.py
@@ -4,12 +4,10 @@ from selenium import webdriver
 driver = webdriver.Remote("http://localhost:4444/wd/hub", webdriver.DesiredCapabilities.FIREFOX.copy())
 driver.get("file://" + os.path.abspath("index.html"))
 print("file://" + os.path.abspath("index.html"));
-# Create a screenshots directory if not present
-# NOTE 2: We are taking screenshots to show CircleCI artifacts
-driver.get_screenshot_as_file('screenshots/test.png')
 
-# Assert the Page Title
-
+driver.get_screenshot_as_file('./screenshots/test.png')
 assert "Shapefile GMaps" in driver.title
-# Close the browser window
+
+driver.find_element_by_id("map")
+
 driver.close()
