b357d57baa2de7bb8e0d5bd8b00c62eb1790bd88
MMartiM <marti.mutso@gmail.com>
2016-10-04 20:18:23 +0300

    test

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,8 @@ import os
 from selenium import webdriver
 
 driver = webdriver.Remote("http://localhost:4444/wd/hub", webdriver.DesiredCapabilities.FIREFOX.copy())
-driver.get("http://www.google.com")
-print("file:///" + os.path.abspath("../index.html"));
+driver.get("file://" + os.path.abspath("index.html"))
+print("file://" + os.path.abspath("index.html"));
 # Create a screenshots directory if not present
 # NOTE 2: We are taking screenshots to show CircleCI artifacts
 driver.get_screenshot_as_file('screenshots/test.png')
