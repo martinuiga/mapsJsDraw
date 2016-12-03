26b2a9238088ad78418adbc04df61439735fe77d
MMartiM <marti.mutso@gmail.com>
2016-12-03 18:37:03 +0200

    1 test added...

--- test/test_browser.js
+++ test/test_browser.js
@@ -1,9 +1,9 @@
 // var expect = chai.expect;
 var assert = chai.assert;
 
-describe("Map", function() {
-  describe("Area", function() {
-    it("must exist on the map after importing", function(done) {
+describe("Map", function () {
+  describe("Area", function () {
+    it("must exist on the map after importing", function (done) {
       this.timeout(2000);
       setTimeout(function () {
         assert.equal(Object.keys(maps._allAreas).length, Object.keys(geo).length, "is equal");
@@ -11,7 +11,7 @@ describe("Map", function() {
       done();
     });
 
-    it("must have 'name' property of 'Test field' after editing metadata", function(done) {
+    it("must have 'name' property of 'Test field' after editing metadata", function (done) {
       this.timeout(3000);
       setTimeout(function () {
         assert.equal(maps._allAreas.features[0]["properties"]["name"], "Test field", "is equal");
@@ -19,7 +19,18 @@ describe("Map", function() {
       done();
     });
 
-    it("should be deleted from the map", function(done) {
+    it("get image URL of the area", function (done) {
+      this.timeout(3000);
+      setTimeout(function () {
+        maps._map.data.forEach(function (feature) {
+          console.log(maps.getScreenshot(feature));
+          assert.equal(maps.getScreenshot(feature) !== "", true, "is equal");
+        }, 3000);
+      });
+      done();
+    });
+
+    it("should be deleted from the map", function (done) {
       this.timeout(4000);
       setTimeout(function () {
         maps.deleteAreas(444, true);
