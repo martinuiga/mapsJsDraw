161f59aebbff96067dc59298f5e46dd37a1d1a48
MMartiM <marti.mutso@gmail.com>
2016-12-19 14:03:12 +0200

    Incomplete tests

--- test/test_browser.js
+++ test/test_browser.js
@@ -1,44 +1,20 @@
-// var expect = chai.expect;
+var expect = chai.expect;
 var assert = chai.assert;
 
 describe("Map", function () {
   describe("Area", function () {
     it("must exist on the map after importing", function (done) {
       this.timeout(2000);
+      testmap1.importGeoJson(geo);
       setTimeout(function () {
-        assert.equal(Object.keys(maps._allAreas).length, Object.keys(geo).length, "is equal");
+        // console.log(JSON.parse(JSON.stringify(geo)));
+        // console.log(JSON.parse(JSON.stringify(testmap1._allAreas)));
+        // assert.deepEqual(JSON.parse(JSON.stringify(testmap1._allAreas)), JSON.parse(JSON.stringify(geo)));
+        assert.deepEqual(testmap1._allAreas, geo);
       }, 2000);
       done();
     });
 
-    it("must have 'name' property of 'Test field' after editing metadata", function (done) {
-      this.timeout(3000);
-      setTimeout(function () {
-        assert.equal(maps._allAreas.features[0]["properties"]["name"], "Test field", "is equal");
-      }, 3000);
-      done();
-    });
-
-    it("get image URL of the area", function (done) {
-      this.timeout(3000);
-      setTimeout(function () {
-        maps._map.data.forEach(function (feature) {
-          console.log(maps.getScreenshot(feature));
-          assert.equal(maps.getScreenshot(feature) !== "", true, "is equal");
-        }, 3000);
-      });
-      done();
-    });
-
-    it("should be deleted from the map", function (done) {
-      this.timeout(4000);
-      setTimeout(function () {
-        maps.deleteAreas(444, true);
-      }, 4000);
-
-      assert.equal(Object.keys(maps._allAreas).length, 0, "is equal");
-      done();
-    });
   });
 
   // describe("#greets", function() {
