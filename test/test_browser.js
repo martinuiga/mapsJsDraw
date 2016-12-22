21f7e389cf30c589e510227821ec462baa045602
MMartiM <marti.mutso@gmail.com>
2016-12-22 17:35:40 +0200

    a fix to infowindow (hover mode), some tests

--- test/test_browser.js
+++ test/test_browser.js
@@ -2,18 +2,34 @@ var expect = chai.expect;
 var assert = chai.assert;
 
 describe("Map", function () {
+  before(function () {
+    shp("../pollud_failide_formaat.zip").then(function (geo) {testmap1.importGeoJson(geo, 'kultuur');});
+    testmap1.setupInfoWindow({'hover':true, 'properties':[{name: 'kultuur', displayName: 'Kultuur'},
+      {name: 'muutja', displayName: 'Muutja123'}]});
+  });
   describe("Area", function () {
-    it("must exist on the map after importing", function (done) {
-      this.timeout(2000);
-      testmap1.importGeoJson(geo);
+    it("infowindow exists on hover", function (done) {
+      this.timeout(300);
       setTimeout(function () {
-        // console.log(JSON.parse(JSON.stringify(geo)));
-        // console.log(JSON.parse(JSON.stringify(testmap1._allAreas)));
-        // assert.deepEqual(JSON.parse(JSON.stringify(testmap1._allAreas)), JSON.parse(JSON.stringify(geo)));
-        assert.deepEqual(testmap1._allAreas, geo);
-      }, 2000);
-      done();
+        testmap1._map.data.forEach(function (feature1) {
+          google.maps.event.trigger(testmap1._map.data, 'mouseover', {feature:feature1})
+        });
+        done();
+      }, 0);
+      setTimeout(function () {
+        assert.equal(!!document.getElementById("infoWindow"), true);
+        done();
+      }, 0);
+
     });
+
+    it("infowindow contains correct data", function (done) {
+      this.timeout(700);
+      setTimeout(function () {
+        assert.equal(document.getElementById("infoWindow").firstChild.getElementsByTagName('b')[0].innerText, "Kultuur:");
+        done();
+      }, 400);
+    })
 
   });
 
