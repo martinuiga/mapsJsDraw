b626d2f2d0de10fe70dd211cbe059111137572f5
MMartiM <marti.mutso@gmail.com>
2016-12-22 20:53:30 +0200

    test fixes

--- test/test_browser.js
+++ test/test_browser.js
@@ -1,48 +1,41 @@
 var expect = chai.expect;
 var assert = chai.assert;
-
 describe("Map", function () {
-  before(function () {
-    shp("../pollud_failide_formaat.zip").then(function (geo) {testmap1.importGeoJson(geo, 'kultuur');});
-    testmap1.setupInfoWindow({'hover':true, 'properties':[{name: 'kultuur', displayName: 'Kultuur'},
-      {name: 'muutja', displayName: 'Muutja123'}]});
+
+  before("asd", function (done) {
+    this.timeout(1000);
+    setTimeout(function () {
+      testmap1.setupInfoWindow({'hover':true, 'properties':[{name: 'kultuur', displayName: 'kultuur'},
+        {name: 'muutja', displayName: 'muutja'}]});
+      shp("../pollud_failide_formaat.zip").then(function (geo) {
+        testmap1.importGeoJson(geo, 'kultuur');
+        done();
+      });
+    }, 500);
+
   });
-  describe("Area", function () {
-    it("infowindow exists on hover", function (done) {
-      this.timeout(300);
-      setTimeout(function () {
-        testmap1._map.data.forEach(function (feature1) {
-          google.maps.event.trigger(testmap1._map.data, 'mouseover', {feature:feature1})
+
+  it("asd", function (done) {
+    describe("Infowindow content checking", function () {
+      testmap1._map.data.forEach(function (feature1) {
+        it("triggers infowindow", function (done) {
+          google.maps.event.trigger(testmap1._map.data, 'mouseover', {feature:feature1});
+          done();
         });
-        done();
-      }, 0);
-      setTimeout(function () {
-        assert.equal(!!document.getElementById("infoWindow"), true);
-        done();
-      }, 0);
+        it("checks infowindow content", function (done) {
+          var text2 = document.getElementById("infoWindow").firstChild.childNodes[1].data;
+          text2 = text2.substr(1);
+          console.log(text2);
+          assert.equal(text2, feature1.getProperty('kultuur'));
+          done();
+        });
+        it("triggers mouseout", function (done) {
+          google.maps.event.trigger(testmap1._map.data, 'mouseout', {feature:feature1});
+          done();
+        });
+      });
 
     });
-
-    it("infowindow contains correct data", function (done) {
-      this.timeout(700);
-      setTimeout(function () {
-        assert.equal(document.getElementById("infoWindow").firstChild.getElementsByTagName('b')[0].innerText, "Kultuur:");
-        done();
-      }, 400);
-    })
-
+    done();
   });
-
-  // describe("#greets", function() {
-  //   it("should throw if no target is passed in", function() {
-  //     expect(function() {
-  //       (new Cow()).greets();
-  //     }).to.throw(Error);
-  //   });
-  //
-  //   it("should greet passed target", function() {
-  //     var greetings = (new Cow("Kate")).greets("Baby");
-  //     expect(greetings).to.equal("Kate greets Baby");
-  //   });
-  // });
 });
