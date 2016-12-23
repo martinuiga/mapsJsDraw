b0a9e1831edabde1caf264b8e1fd4b6ddec6619d
MMartiM <marti.mutso@gmail.com>
2016-12-23 10:46:51 +0200

    various fixes and tests

--- test/test_browser.js
+++ test/test_browser.js
@@ -5,8 +5,9 @@ describe("Map", function () {
   before("asd", function (done) {
     this.timeout(1000);
     setTimeout(function () {
-      testmap1.setupInfoWindow({'hover':true, 'properties':[{name: 'kultuur', displayName: 'kultuur'},
-        {name: 'muutja', displayName: 'muutja'}]});
+      testmap2.importGeoJson(geo, 'id');
+      testmap1.setupInfoWindow({'hover':true, 'properties':[{name: 'kultuur', displayName: 'kultuur'}]});
+      testmap2.setupInfoWindow({'hover':false, 'editable':true, 'properties':[{name: 'id', displayName: 'id'}]});
       shp("../pollud_failide_formaat.zip").then(function (geo) {
         testmap1.importGeoJson(geo, 'kultuur');
         done();
@@ -15,7 +16,7 @@ describe("Map", function () {
 
   });
 
-  it("asd", function (done) {
+  it("checks stuff", function (done) {
     describe("Infowindow content checking", function () {
       testmap1._map.data.forEach(function (feature1) {
         it("triggers infowindow", function (done) {
@@ -38,4 +39,49 @@ describe("Map", function () {
     });
     done();
   });
+  it("changes area property", function (done) {
+    testmap2.editProperties(444, 'propp', 'valuee');
+    done();
+  });
+  it('groups by new property', function (done) {
+    testmap2.groupAreasByProperty('propp');
+    done();
+  });
+  it('displays areas by property', function (done) {
+    testmap1.displayAreasByProperty({'kultuur':['pÃµlduba, v.a "JÃµgeva"', 'roosa ristik (100% ristikut)']});
+    done();
+  });
+  it('displays all areas', function (done) {
+    testmap1.displayAllAreas();
+    done();
+  });
+  it('selects an area', function (done) {
+    testmap2.selectArea(444);
+    done();
+  });
+  it('saves areas to localStorage', function (done) {
+    testmap2.saveDataToLocalStorage();
+    done();
+  });
+  it('deletes an area', function (done) {
+    testmap2.deleteAreas(testmap2.getSelectedAreas()[0], true);
+    done();
+  });
+  it('loads areas from localStorage', function (done) {
+    testmap2.loadDataFromLocalStorage();
+    done();
+  });
+  it('imports an area with same id', function (done) {
+    testmap2.importGeoJson(geo2);
+    testmap2.groupAreasByProperty('id');
+    done();
+  });
+  it('loads again from localStorage', function (done) {
+    testmap2.loadDataFromLocalStorage();
+    done();
+  });
+  it('adds a marker', function (done) {
+    testmap1.addMarker(27.216284488869114,58.26829064495879, {"siin": "on üks rida", "siin2": "on teine rida"});
+    done();
+  })
 });
